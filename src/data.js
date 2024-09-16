dd06715b8210031060b4b82be840874df4d1e6ed
unknown <Junior-d09>
2024-09-16 18:32:31 +0100

    init

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://img.freepik.com/premium-photo/asian-beautiful-woman-isolated-blue-wall_171337-49662.jpg?ga=GA1.1.1853029138.1724930229&semt=ais_hybrid",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://img.freepik.com/free-photo/shirt-hanger-with-green-background_23-2150264156.jpg?t=st=1724949871~exp=1724953471~hmac=5f5ec0ae6b38f902ffb2ad957277601c978afccbc6b5e76b25b1869998601262&w=740",
+    },
+    {
+      id:3,
+      img:"https://img.freepik.com/premium-photo/3d-illustration-shirt_874909-5153.jpg?w=740",
+    },
+    {
+      id:4,
+      img:"https://img.freepik.com/psd-gratuit/vue-laterale-maquette-capuche-pour-hommes_1332-60624.jpg?t=st=1724952109~exp=1724955709~hmac=1da898681a07eec935fc98fe82fc671b74a891bb15e615b5f7cf5df820b408af&w=740",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://img.freepik.com/free-photo/fashion-woman-with-clothes_1203-8302.jpg?t=st=1724952223~exp=1724955823~hmac=4fd4156beb45036eeeed83760291b0c51fbad8e784686c60d1216e2ffe61baf5&w=740",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
